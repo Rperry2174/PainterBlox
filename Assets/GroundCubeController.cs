4cba44b40a4d33f078228b3b0647cfa9a816d0ff
Ryan Perry <Rperry2174@gmail.com>
2018-10-14 18:38:34 -0700

    Add project to github

---
+++ Assets/GroundCubeController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCubeController : MonoBehaviour {
+	public Color originalBoxColor = new Color(140f, 101f, 51f);
+	public Color triggerBoxColor = new Color(0f, 0f, 255f);
+	public BoxCollider triggerCollider;
+    public BoxCollider groundCollider;
+
+	// Use this for initialization
+	void Start () {
+		gameObject.GetComponent<Renderer>().material.color = originalBoxColor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void ChangeColor (Color newColor) {
+		gameObject.GetComponent<Renderer>().material.color = newColor;
+	}
+
+	private void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("entered");
+		ChangeColor(triggerBoxColor);
+    }
+
+	private void OnTriggerExit(Collider other)
+    {
+		Debug.Log("entered");
+		ChangeColor(originalBoxColor);
+    }
+
+
+}
---
+++ Assets/SphereHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+	public class SphereHandler : MonoBehaviour {
+
+	protected Joystick joystick;
+	protected Joybutton joybutton;
+    
+	protected bool jump;
+	public float velocityFactor = 2.5f;
+	public float speed;
+	
+	// Use this for initialization
+	void Start () {
+		joystick = FindObjectOfType<Joystick>();
+		joybutton = FindObjectOfType<Joybutton>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var rb = GetComponent<Rigidbody>();
+		rb.velocity = new Vector3(joystick.Horizontal * velocityFactor,
+										 rb.velocity.y,
+		                          joystick.Vertical * velocityFactor);
+
+		float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+        if(!jump && joybutton.Pressed)
+		{
+			jump = true;
+			rb.velocity += Vector3.up * velocityFactor;
+		}
+
+		if(jump && !joybutton.Pressed)
+		{
+			jump = false;
+		}
+	}
+}
