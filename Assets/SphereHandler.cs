4a32c0e9d642a6d38cc6efd54085a725a7eac3fe
Ryan Perry <Rperry2174@gmail.com>
2018-10-16 22:23:30 -0700

    Add rotation to player cube

--- Assets/SphereHandler.cs
+++ Assets/SphereHandler.cs
@@ -20,16 +20,20 @@ using UnityEngine;
 	// Update is called once per frame
 	void Update () {
 		var rb = GetComponent<Rigidbody>();
-		rb.velocity = new Vector3(joystick.Horizontal * velocityFactor,
-										 rb.velocity.y,
-		                          joystick.Vertical * velocityFactor);
+        Vector3 dir = new Vector3(joystick.Horizontal * velocityFactor,
+                                  rb.velocity.y,
+                                     joystick.Vertical * velocityFactor);
+		
+		rb.velocity = dir;
 
 		float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
-        rb.AddForce(movement * speed);
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);        
+		rb.AddForce(movement * speed);
+        
+		Debug.Log("gameobject" + dir);
+		transform.rotation = Quaternion.LookRotation(dir);
 
         if(!jump && joybutton.Pressed)
 		{
@@ -41,5 +45,34 @@ using UnityEngine;
 		{
 			jump = false;
 		}
+
+		//Twist();
 	}
+
+	void Twist()
+    {
+		float h1 = Input.GetAxis("Horizontal");
+        float v1 = Input.GetAxis("Vertical");
+
+        if (h1 == 0f && v1 == 0f)
+        { // this statement allows it to recenter once the inputs are at zero 
+			Vector3 curRot = gameObject.transform.localEulerAngles; // the object you are rotating
+            Vector3 homeRot;
+            if (curRot.y > 180f)
+            { // this section determines the direction it returns home 
+                Debug.Log(curRot.y);
+                homeRot = new Vector3(0f, 359.999f, 0f); //it doesnt return to perfect zero 
+            }
+            else
+            {                                                                      // otherwise it rotates wrong direction 
+                homeRot = Vector3.zero;
+            }
+			gameObject.transform.localEulerAngles = Vector3.Slerp(curRot, homeRot, Time.deltaTime * 2);
+        }
+        else
+        {
+			Debug.Log("gameobject" + gameObject.transform.rotation);         
+			gameObject.transform.localEulerAngles = new Vector3(0f, Mathf.Atan2(h1, v1) * 180 / Mathf.PI, 0f); // this does the actual rotaion according to inputs
+        }
+    }
 }
