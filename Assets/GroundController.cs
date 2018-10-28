d74570ea990d713e39f4a18bda9db17fae17c3dc
Ryan Perry <Rperry2174@gmail.com>
2018-10-27 20:47:17 -0700

    Adds working blocks fall and respawn

---
+++ Assets/GroundController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundController : MonoBehaviour {
+
+	public GameObject[,] objects;
+	public GameObject groundCube;
+    
+	// Use this for initialization
+	void Start () {
+		objects = new GameObject[8, 8];
+
+		for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+				objects[i, j] = (GameObject)Instantiate(groundCube, new Vector3(i, 0, j), Quaternion.identity);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
--- Assets/GroundCubeController.cs
+++ Assets/GroundCubeController.cs
@@ -3,19 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GroundCubeController : MonoBehaviour {
+	public Vector3 originalBoxPosition;
 	public Color originalBoxColor = new Color(140f, 101f, 51f);
 	public Color triggerBoxColor = new Color(0f, 0f, 255f);
 	public BoxCollider triggerCollider;
     public BoxCollider groundCollider;
+	public bool isFalling = false;
 
 	// Use this for initialization
 	void Start () {
 		gameObject.GetComponent<Renderer>().material.color = originalBoxColor;
+		originalBoxPosition = gameObject.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (isFalling)
+		{
+			gameObject.transform.Translate(Vector3.down * Time.deltaTime);
+		}
 	}
 
 	void ChangeColor (Color newColor) {
@@ -24,7 +30,11 @@ public class GroundCubeController : MonoBehaviour {
 
 	private void OnTriggerEnter(Collider col)
     {
-        //Debug.Log("entered");
+		if(col.gameObject.tag == "Bullet")
+		{
+			isFalling = true;
+			StartCoroutine(Respawn());
+		}
 		ChangeColor(triggerBoxColor);
     }
 
@@ -34,5 +44,12 @@ public class GroundCubeController : MonoBehaviour {
 		ChangeColor(originalBoxColor);
     }
 
+	IEnumerator Respawn()
+	{
+		//print("start: " + Time.time);
+        yield return new WaitForSeconds(5);
+		isFalling = false;
+		gameObject.transform.position = originalBoxPosition;
+	}
 
 }
