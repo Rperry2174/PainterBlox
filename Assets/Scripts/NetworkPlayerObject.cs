ce875d6c2a5afaa8d2d044ebb0e5a4fb6e93c941
Ryan Perry <Rperry2174@gmail.com>
2018-10-30 01:11:10 -0700

    Add working networking

---
+++ Assets/Scripts/NetworkPlayerObject.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkPlayerObject : NetworkBehaviour {
+
+	public GameObject PlayerUnitPrefab;
+
+    // Use this for initialization
+    void Start () {
+		if( isLocalPlayer == false )
+        {
+			Debug.Log("PlayerOBject::Start -- isLocalPlayer === false");
+
+			// This belongs to someone else
+            return;
+        }
+        // Since the PlayerObject is invisible give me something physical to move around
+
+        //Debug.Log("PlayerObject::Start -- Spawining my own personal unit.");
+		//Instantiate(PlayerUnitPrefab);
+
+		// Command the server to SPAWN out unit
+		CmdSpawnMyUnit();
+    }
+
+    // Update is called once per frame
+	void Update () {
+        
+	}
+
+    // Commands to be run ONLY on the server
+    [Command]
+	void CmdSpawnMyUnit()
+	{
+		Debug.Log("CmdSpawnMyUnit -- Spawining my own personal unit.");
+
+		Vector3 spawnPosition = new Vector3(1.0f, 1.0f, 1.0f);
+        
+		// We are guarenteed to be on the server now
+		GameObject go = Instantiate(PlayerUnitPrefab);
+
+		// Now that the object exists on the server propogate it to all 
+		// the clients (and also wire up the NetworkIdentity)
+		NetworkServer.Spawn(go);
+	}
+
+}
---
+++ Assets/Scripts/PlayerObject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerObject : NetworkBehaviour {
+
+	public GameObject PlayerUnitPrefab;
+
+	// Use this for initialization
+	void Start () {
+
+		if (isLocalPlayer == false)
+		{
+			return;
+		}
+		// Since the playerOBject is invisibile and not part of the world,
+		// give me something physical to move around!
+		//Debug.Log("PlayerOBject::start -- Spawning my own personal unit.");
+		// Instantiate(PlayerUnitPrefab, new Vector3(2.0f, 2.0f, 2.0f), gameObject.transform.rotation);
+		CmdSpawnMyUnit();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	[Command]
+	void CmdSpawnMyUnit() {
+		Debug.Log("CmdSpawnMyUnit() -- Spawning my own personal unit.");
+
+		GameObject go = Instantiate(PlayerUnitPrefab, new Vector3(2.0f, 2.0f, 2.0f), gameObject.transform.rotation);
+
+		// Now that the object exists on the server, propagate it to all
+		// the clients (and also wire up the NetworkIdentity)
+
+		NetworkServer.Spawn(go);
+
+	}
+}
